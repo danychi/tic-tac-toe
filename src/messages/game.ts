d67c103162308930bb3d6be915d4379745d31ce2
Daniel Alarcón Amador <danychi@Daniels-MacBook-Pro.local>
2021-11-02 14:10:30 +0100

    feat: 🎸 Add the messages for the app

---
+++ src/messages/game.ts
@@ -0,0 +1,20 @@
+const GAME_MESSAGES = {
+  intro:
+    'Welcome to Tic Tac Toe! I hope you have a lot of fun playing this little simple game 👾',
+  rulesTitle: 'RULES FOR TIC-TAC-TOE',
+  rule1: 'The game is normally played on a grid of 3 squares by 3 squares.',
+  rule2:
+    'You are X, your friend is O. Players take turns putting their marks in empty squares.',
+  rule3:
+    'The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.',
+  rule4:
+    'When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.',
+  goodLuck: 'I wish you good luck, and enjoy the game! 😊',
+  startButton: 'Click to start',
+  restartButton: 'Click to restart the game',
+  win: 'HOORAAAY!!! PLAYER ${player} HAS WON 🥳🎉💐🎁🏆',
+  draw: "IT'S A DRAW! PLAY ANOTHER GAME TO FIND A WINNER",
+  nextTurn: 'PLAYER ${player} YOU ARE NEXT!',
+}
+
+export { GAME_MESSAGES }
