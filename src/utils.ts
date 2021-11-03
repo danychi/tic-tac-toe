307c02d743d2cdafe83e10f8df7e4b02193f34b4
Daniel Alarcón Amador <danychi@Daniels-MacBook-Pro.local>
2021-11-03 10:58:33 +0100

    feat: 🎸 improve the calculateDidSomeoneWin function

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,33 @@
 import { PlayType } from './enums'
 
+type Triplet = [number, number, number]
+
+const horizontal: Triplet[] = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+]
+
+const vertical: Triplet[] = [
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+]
+
+const diagonal: Triplet[] = [
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 const calculateDidSomeoneWin = (board: PlayType[]): boolean => {
-  const winningColumns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
-  // checking each of the positions seeing if any of the two players won
-  for (let i = 0; i < winningColumns.length; i++) {
-    const [a, b, c] = winningColumns[i]
-    if (board[a] !== '' && board[a] === board[b] && board[a] === board[c]) {
-      return true
-    }
-  }
-  return false
+  const verification = ([a, b, c]: Triplet): boolean =>
+    board[a] !== '' && board[a] === board[b] && board[a] === board[c]
+  // The calculation will stop in the moment that the horizontal/vertical/diagonal winning verification is true
+  return (
+    horizontal.some(verification) ||
+    vertical.some(verification) ||
+    diagonal.some(verification)
+  )
 }
 
 export { calculateDidSomeoneWin }
